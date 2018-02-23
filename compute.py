15c71dc503cb2b527314963a7f114f4d58ce9d42
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-02-23 11:49:10 +0100

    TST solve issue with zero division

--- compute.py
+++ compute.py
@@ -1,8 +1,14 @@
 from __future__ import division
 
+import numpy as np
+
 
 def divide(a, b):
-    return a / b
+    try:
+        x = a / b
+    except ZeroDivisionError:
+        x = np.inf
+    return x
 
 
 def multiply(a, b):
